cc9ce0e485444e4f26f4603634026f9b4970c1d2
Sergei Saliba <sergei.saliba@cleverbit.com>
2018-10-05 10:49:41 +0200

    Add project files.

---
+++ FullStackExcercise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FullStackExcercise.MvcApplication" Language="C#" %>
---
+++ FullStackExcercise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FullStackExcercise.Startup))]
+namespace FullStackExcercise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
