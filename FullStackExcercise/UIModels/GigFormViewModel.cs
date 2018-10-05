1e71968a4b10219010a6a129a91c79280edd5a6e
Sergei Saliba <sergei.saliba@cleverbit.com>
2018-10-05 15:27:04 +0200

    Added validation for time

--- FullStackExcercise/UIModels/GigFormViewModel.cs
+++ FullStackExcercise/UIModels/GigFormViewModel.cs
@@ -1,17 +1,32 @@
 ﻿using FullStackExcercise.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FullStackExcercise.UIModels
 {
     public class GigFormViewModel
     {
-        public DateTime DateTime => DateTime.Parse($"{Date} {Time}");
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse($"{Date} {Time}");
+        }
 
+
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [ValidTime]
+        [Required]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
     }
 }
