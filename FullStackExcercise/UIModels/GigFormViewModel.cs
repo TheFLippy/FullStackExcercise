162edaac48b06de6e34ef898c79bc81e57539bbd
Sergei Saliba <sergei.saliba@cleverbit.com>
2018-10-05 12:33:12 +0200

    Added create page for Gigs

---
+++ FullStackExcercise/UIModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FullStackExcercise.Models;
+using System.Collections.Generic;
+
+namespace FullStackExcercise.UIModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
