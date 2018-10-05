42a307d87278eb5864c169bad8e5a72c75cd9972
Sergei Saliba <sergei.saliba@cleverbit.com>
2018-10-05 11:27:05 +0200

    Added new models Gig and Genre and fixed DB settings by adding Data Annotations

---
+++ FullStackExcercise/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FullStackExcercise.Models
+{
+    public class Genre
+    {
+        public Byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
